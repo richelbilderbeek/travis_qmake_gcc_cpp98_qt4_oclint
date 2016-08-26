bcc3903524f06b43d867ce4844aa478fd443a220
richelbilderbeek <richel@richelbilderbeek.nl>
2016-08-26 12:04:37 +0200

    OCLint works correctly

--- do_oclint.sh
+++ do_oclint.sh
@@ -3,27 +3,17 @@
 cpp_files=`ls *.cpp | egrep -v "^qrc_.*\.cpp$" | egrep -v "^moc_.*\.cpp$"`
 h_files=`ls *.h | egrep -v "^ui_.*\.h$"`
 
-./oclint-0.10.3/bin/oclint -o oclint.log -max-priority-1 0 -max-priority-2 0 -max-priority-3 0 \
+./oclint-0.10.3/bin/oclint -o oclint.log \
   -disable-rule ShortVariableName \
-  -disable-rule FileNotFound \
   $cpp_files \
   $h_files \
   -- \
-  -c -std=c++11 \
-  -I../RibiClasses/CppAbout \
-  -I../RibiClasses/CppHelp \
-  -I../RibiClasses/CppTestTimer \
-  -I../RibiClasses/CppFileIo \
-  -I../RibiClasses/CppMenuDialog \
-  -I../RibiClasses/CppQtAboutDialog \
-  -I../RibiClasses/CppQtHideAndShowDialog \
-  -I../RibiClasses/CppTrace \
+  -c -std=c++98 \
   -I/usr/include/c++/5 \
-  -I/usr/include/qt5 \
-  -I/usr/include/qt5/QtCore \
-  -I/usr/include/qt5/QtGui \
-  -I/usr/include/qt5/QtWidgets \
-  > /dev/null
+  -I/usr/include/x86_64-linux-gnu/c++/5 \
+  -I/usr/include/qt4 \
+  -I/usr/include/qt4/QtCore \
+  -I/usr/include/qt4/QtGui
 
 # Will be 1 if success
 # Will be 0 if fail
@@ -32,6 +22,7 @@ success=`egrep "FilesWithViolations=0 P1=0 P2=0 P3=0" oclint.log | wc -l`
 if [ $success -eq 1 ]; 
 then
   echo "OCLint: OK"
+  cat oclint.log
 else
   echo "OCLint: Fail"
   cat oclint.log
